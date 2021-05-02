3fc76d11c6c67ca722733ffee12ccb75564911f0
Roopaa208 <45393371+Roopaa208@users.noreply.github.com>
2021-05-02 21:27:34 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const addUser = document.getElementById("user-btn");
 //const mainApp = document.getElementById("app");
 const userList = document.getElementById("user-list");
 const searchInput = document.getElementById("search");
-const sortBtn = document.getElementById("sort");
+const descsortBtn = document.getElementById("sort-desc");
+const ascsortBtn = document.getElementById("sort-asc");
 const appState = []
+
+class User{
+  constructor(title,firstname,lastname,gender,email){
+    this.name = `${title} ${firstname} ${lastname}`
+    this.gender = gender
+    this.email = email
+  }
+}
 
 
 addUser.addEventListener('click', async () => {
@@ -13,7 +22,14 @@ addUser.addEventListener('click', async () => {
   });
   const userJson = await userData.json()
   const user = userJson.results[0]
-  appState.push(user)
+  const classUser = new User(
+    user.name.title, 
+    user.name.first, 
+    user.name.last, 
+    user.gender,
+    user.email
+  );
+  appState.push(classUser)
   console.log(appState)
   domRenderer(appState)
 });
@@ -23,7 +39,7 @@ const domRenderer = (stateArr) => {
   stateArr.forEach(userObj => {
     const userEl = document.createElement('div')
     userEl.innerHTML = `<div>
-    Name: ${userObj.name.title} ${userObj.name.first} ${userObj.name.last}
+    Name: ${userObj.name}
     <ol>
       <li>${userObj.gender}</li>
       <li>${userObj.email}</li>
@@ -36,9 +52,7 @@ const domRenderer = (stateArr) => {
 searchInput.addEventListener("keyup",(e) => {
   console.log(e,searchInput.value);
   const filterAppState = appState.filter(user =>
-    user.name.first.toLowerCase()
-    .includes(searchInput.value.toLowerCase()) ||
-    user.name.last.toLowerCase()
+    user.name.toLowerCase()
     .includes(searchInput.value.toLowerCase()) ||
     user.gender.toLowerCase()
     .includes(searchInput.value.toLowerCase()) ||
@@ -48,11 +62,17 @@ searchInput.addEventListener("keyup",(e) => {
   domRenderer(filterAppState)
 });
 
-sortBtn.addEventListener('click',() => {
+
+descsortBtn.addEventListener('click',() => {
   const appStateCopy = [...appState]
-  appStateCopy.sort((a,b) => a.name.first - b.name.first)
+  appStateCopy.sort((a,b) => a.name < b.name ? 1: -1)
   
   domRenderer(appStateCopy)
 });
 
-
+ascsortBtn.addEventListener('click',() => {
+  const appStateCopy = [...appState]
+  appStateCopy.sort((a,b) => a.name < b.name ? -1: 1)
+  
+  domRenderer(appStateCopy)
+});
